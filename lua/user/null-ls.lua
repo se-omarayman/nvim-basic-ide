fe630060bc7bfd170b8a6171fcc97e19f4b9a627
Omar Ayman <se.omarayman@gmail.com>
2023-04-24 05:19:16 +0200

    updated null ls to use eslintd for formatting and diagnostics

--- lua/user/null-ls.lua
+++ lua/user/null-ls.lua
@@ -22,10 +22,12 @@ function M.config()
   null_ls.setup {
     debug = false,
     sources = {
-      formatting.prettier.with {
-        extra_filetypes = { "toml" },
-        extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-      },
+      -- formatting.prettier.with {
+      --   extra_filetypes = { "toml" },
+      --   extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+      -- },
+      formatting.eslint_d,
+      diagnostics.eslint_d,
       formatting.black.with { extra_args = { "--fast" } },
       formatting.stylua,
       formatting.google_java_format,
