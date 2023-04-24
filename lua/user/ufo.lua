f940a2fd3eb87d46654c5eaec0aff2b6c8aead25
Omar Ayman <se.omarayman@gmail.com>
2023-04-24 05:10:07 +0200

    added git-conflict and updated lsp hover shortcut

---
+++ lua/user/ufo.lua
@@ -0,0 +1,11 @@
+local M = { "kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async" }
+
+function M.config()
+  local ufo = require("ufo")
+  ufo.setup {
+    provider_selector = function(bufnr, filetype, buftype)
+      return { "treesitter", "indent" }
+    end,
+  }
+end
+return M
