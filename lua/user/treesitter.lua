db6d8f624e4f3de2e9b9f42caf4e5f6c22f33e1a
Omar Ayman <se.omarayman@gmail.com>
2023-04-24 05:31:27 +0200

    updates

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -21,6 +21,7 @@ function M.config()
 
   configs.setup {
     ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python" }, -- put the language you want in this array
+    auto_install = true,
     -- ensure_installed = "all", -- one of "all" or a list of languages
     ignore_install = { "" },                                                       -- List of parsers to ignore installing
     sync_install = false,                                                          -- install languages synchronously (only applied to `ensure_installed`)
