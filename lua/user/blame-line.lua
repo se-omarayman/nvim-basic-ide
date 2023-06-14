0eb02fbd61720f81a9af72c19a0b5d2af07342bd
Omar Ayman <se.omarayman@gmail.com>
2023-06-14 16:05:24 +0300

    add blame_line

---
+++ lua/user/blame-line.lua
@@ -0,0 +1,11 @@
+local M = {
+  "braxtons12/blame_line.nvim",
+  event = "BufReadPre",
+}
+
+-- function M.config()
+--   local blame = require("nvim-blame-line")
+--   blame.setup {}
+-- end
+
+return M
