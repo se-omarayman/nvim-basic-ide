63779a454bfcdb25300adc35da7b02bca37b7b52
Omar Ayman <se.omarayman@gmail.com>
2023-08-27 20:03:01 +0300

    update keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -17,10 +17,10 @@ vim.g.mapleader = " "
 
 -- Normal --
 -- Better window navigation
-keymap("n", "<C-j>", "<C-w>h", opts)
-keymap("n", "<C-h>", "<C-w>j", opts)
-keymap("n", "<C-l>", "<C-w>k", opts)
-keymap("n", "<C-k>", "<C-w>l", opts)
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
